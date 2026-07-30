7e0a354ab07dc9422b480db863dcc3405b8d469e
Muhammed-maher32 <muhammedmaher916@gmail.com>
2026-07-31 01:23:24 +0300

    AddInterceptor

--- ECommerce.Infrastructure/DependencyInjection.cs
+++ ECommerce.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using ECommerce.Infrastructure.Persistence.DbContexts;
+﻿using ECommerce.Infrastructure.Interceptors;
+using ECommerce.Infrastructure.Persistence.DbContexts;
 using ECommerce.Infrastructure.Persistence.Seeding;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -11,16 +12,18 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services,
         IConfiguration config)
     {
-        services.AddDbContext<StoreDbContext>(options =>
+        services.AddDbContext<StoreDbContext>((sp, options) =>
         {
             options.UseNpgsql(config.GetConnectionString("DefaultConnection"))
-            .EnableSensitiveDataLogging();
+            .EnableSensitiveDataLogging()
+            .AddInterceptors(sp.GetRequiredService<AuditInterceptor>());
         });
         services.AddScoped<IDataseeder, ProductBrandSeeder>();
         services.AddScoped<IDataseeder, ProductTypeSeeder>();
         //Since (IEnumerable<IDataseeder>seeders)
         //Its IEnumerable it will send em in order cuz its ienumerable not just a single object
         services.AddScoped<DataBaseSeeder>();
+        services.AddScoped<AuditInterceptor>();
         return services;
     }
 }
--- ECommerce.Infrastructure/Interceptors/AuditInterceptor.cs
+++ ECommerce.Infrastructure/Interceptors/AuditInterceptor.cs
@@ -1,8 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace ECommerce.Infrastructure.Interceptors;
 
 public class AuditInterceptor : SaveChangesInterceptor
 {
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
 
+    {
+        var context = eventData.Context;
+
+        if (context is null)
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+
+        var entries = context.ChangeTracker.Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Added ||
+            e.State == EntityState.Modified);
+
+        var now = DateTimeOffset.UtcNow;
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(x => x.CreatedAt).CurrentValue = now;
+            }
+            else
+            {
+                entry.Property(x => x.UpdatedAt).CurrentValue = now;
+            }
+        }
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
 }
