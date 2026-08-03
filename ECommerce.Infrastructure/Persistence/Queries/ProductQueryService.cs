153b8ed5c56ac2bf1df2f781b1695592bb5c145c
Muhammed-maher32 <muhammedmaher916@gmail.com>
2026-08-03 16:54:01 +0300

    Upload&Delete PhotoService

--- ECommerce.Infrastructure/Persistence/Queries/ProductQueryService.cs
+++ ECommerce.Infrastructure/Persistence/Queries/ProductQueryService.cs
@@ -1,4 +1,4 @@
-﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Entities;
 using ECommerce.Infrastructure.Persistence.DbContexts;
 using ECommerce.UseCases.Products;
 using ECommerce.UseCases.Products.Dtos;
@@ -9,14 +9,6 @@ namespace ECommerce.Infrastructure.Persistence.Queries;
 
 public class ProductQueryService(StoreDbContext dbContext) : IProductQueryService
 {
-    public async Task<IReadOnlyList<GetAllProductsResponse>> GetAllProductsAsync(CancellationToken cancellationToken = default)
-    {
-        return await dbContext.Products
-            .AsNoTracking()
-            .ProjectToType<GetAllProductsResponse>()
-            .ToListAsync(cancellationToken);
-    }
-
     public async Task<GetByIdProductResponse?> GetByIdProductAsync(Guid id, CancellationToken cancellationToken = default)
     {
         return await dbContext.Products
--- ECommerce.UseCases/Products/Queries/GetAllProductsQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using ECommerce.Domain.Common;
-using ECommerce.UseCases.Products.Dtos;
-using MediatR;
-
-namespace ECommerce.UseCases.Products.Queries;
-
-public sealed record GetAllProductsQuery() : IRequest<Result<IReadOnlyList<GetAllProductsResponse>>>;
-
